b5fc3b02ec5e268d80c03fb6faa88e62326fa1c3
Pankil <61769666+pankildoshi@users.noreply.github.com>
2021-08-05 21:13:06 +0530

    Delete A directory

--- A/app.js
+++
@@ -1,8 +0,0 @@
-const url = 'https://api.themoviedb.org/3/movie/550?api_key=fa7ebbbcacc3a1b2d320e817b3a2534b';
-
-fetch(url).then(
-    response => response.json()
-).then(
-    data => console.log(data)
-);
-
